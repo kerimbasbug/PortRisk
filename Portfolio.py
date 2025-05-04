fe2726eccdd1e5b7e2979daa29bba803f31f5b4b
Kerim Başbuğ <40834300+kerimbasbug@users.noreply.github.com>
2025-05-04 15:52:26 +0200

    BackupPortolio class added.

--- Portfolio.py
+++ Portfolio.py
@@ -184,3 +184,140 @@ class Portfolio:
         # Display the plot using Streamlit
         st.pyplot(fig)
 
+
+class BackupPortfolio:
+    def __init__(self, tickers, weights, start_date, end_date, confidence_level=0.95, n_days=1, calculate_cvar=True, distribution="Normal"):
+        self.tickers = tickers
+        self.weights = np.array(weights)
+        self.confidence_level = confidence_level
+        self.start_date = start_date
+        self.end_date = end_date
+        self.n_days = n_days
+        self.calculate_cvar = calculate_cvar
+        self.distribution = distribution
+        self.data = pd.read_csv('Data/backup_data.csv', index_col=0, parse_dates=True)
+        self.portfolio_returns = self.calculate_portfolio_returns()
+        self.portfolio_mean = self.portfolio_returns.mean()
+        self.portfolio_std = self.calculate_port_std()
+
+    def calculate_portfolio_returns(self):
+        """Calculate daily returns for the portfolio based on asset weights and returns."""
+        if self.data is None:
+            self.error_message = "Data could not be downloaded. Please check the tickers and date range."
+            return None
+        
+        # Calculate daily returns
+        daily_returns = self.data.pct_change().dropna()
+        
+        if daily_returns.empty:
+            self.error_message = "The data is insufficient for calculating daily returns. Please check the tickers and date range."
+            return None
+        
+        if daily_returns.isnull().values.any():
+            self.error_message = "The calculated daily returns contain missing values."
+            return None
+        
+        # Calculate portfolio returns
+        portfolio_returns = daily_returns.dot(self.weights)
+        return portfolio_returns
+
+    def calculate_port_std(self):
+        covmat = self.data.pct_change().dropna().cov()
+        return np.sqrt(np.dot(self.weights.T ,np.dot(covmat, self.weights)))
+
+    def historical_var(self):
+        """Calculate VaR using Historical Simulation (in percentage terms) for n days."""
+        var_percent = np.percentile(self.portfolio_returns, (1 - self.confidence_level) * 100)
+        return var_percent * 100 * np.sqrt(self.n_days)
+
+    def historical_cvar(self):
+        """Calculate CVaR using Historical Simulation (in percentage terms) for n days."""
+        var_threshold = np.percentile(self.portfolio_returns, (1 - self.confidence_level) * 100)
+        cvar_returns = self.portfolio_returns[self.portfolio_returns <= var_threshold]
+        return cvar_returns.mean() * 100 * np.sqrt(self.n_days)
+
+
+    def parametric_var(self):
+        """Calculate VaR using Parametric method (in percentage terms) for n days."""
+        mean = self.portfolio_returns.mean()
+        print('mean', mean)
+        std_dev = self.calculate_port_std()#self.portfolio_returns.std()
+        print('std', std_dev)
+        print('ci', 1-self.confidence_level)
+        if self.distribution == 'Normal':
+            var_percent = norm.ppf(self.confidence_level) * std_dev * np.sqrt(self.n_days) - mean * self.n_days
+        elif self.distribution == 'Student-t':
+            dof, _, _ = t.fit(self.portfolio_returns)
+            dof = np.round(dof)
+            print(t.ppf(self.confidence_level, dof), "HERE")
+            var_percent = np.sqrt((dof-2)/dof) * t.ppf(self.confidence_level, dof) * std_dev * np.sqrt(self.n_days) - mean * self.n_days
+        else:
+            raise ValueError('Pick either "normal" or "t"')
+        return var_percent * 100
+
+    def parametric_cvar(self):
+        """Calculate CVaR (Conditional Value at Risk) using the parametric method for n days."""
+        mean = self.portfolio_returns.mean()
+        std_dev = self.calculate_port_std()#self.portfolio_returns.std()
+        
+        if self.distribution == 'Normal':
+            z_alpha = norm.ppf(1 - self.confidence_level)
+            cvar_percent = (norm.pdf(z_alpha) / (1 - self.confidence_level)) * std_dev * np.sqrt(self.n_days) - mean * self.n_days
+
+        elif self.distribution == 'Student-t':
+            dof, _, _ = t.fit(self.portfolio_returns)
+            dof = np.round(dof)
+            xanu = t.ppf(1 - self.confidence_level, dof)
+            cvar_percent = (-1 / (1 - self.confidence_level)) * (1 / (1 - dof)) * (dof - 2 + xanu**2) * t.pdf(xanu, dof) * std_dev * np.sqrt(self.n_days) - mean * self.n_days
+        else:
+            raise ValueError('Pick either "normal" or "t" for the distribution parameter.')
+        
+        return cvar_percent * 100
+
+    def monte_carlo_cvar(self, simulations=10000):
+        """Calculate CVaR using Monte Carlo Simulation (in percentage terms) for n days."""
+        mean = self.portfolio_returns.mean()
+        std_dev = self.calculate_port_std()#self.portfolio_returns.std()
+        
+        # Simulate portfolio returns
+        simulated_returns = np.random.normal(mean * self.n_days, std_dev * np.sqrt(self.n_days), simulations)
+        
+        # Calculate VaR at the given confidence level
+        var_percent = np.percentile(simulated_returns, (1 - self.confidence_level) * 100)
+        
+        # Calculate CVaR, which is the average of the worst losses (those worse than VaR)
+        cvar_percent = simulated_returns[simulated_returns <= var_percent].mean()
+        
+        return cvar_percent * 100
+    
+    def monte_carlo_var(self, simulations=10000):
+        """Calculate VaR using Monte Carlo Simulation (in percentage terms) for n days."""
+        mean = self.portfolio_returns.mean()
+        std_dev = self.calculate_port_std()# self.portfolio_returns.std()
+        simulated_returns = np.random.normal(mean * self.n_days, std_dev * np.sqrt(self.n_days), simulations)
+        var_percent = np.percentile(simulated_returns, (1 - self.confidence_level) * 100)
+        return var_percent * 100
+
+    def plot_returns_distribution(self):
+        """Plot the distribution of portfolio returns with VaR thresholds highlighted."""
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.hist(self.portfolio_returns, bins=50, alpha=0.5, color="blue", edgecolor="black", density=True, label="Portfolio Returns")
+
+        # Calculate and plot VaR threshold lines
+        historical_var = self.historical_var()
+        parametric_var_normal = self.parametric_var()
+        parametric_var_t = self.parametric_var()
+        monte_carlo_var = self.monte_carlo_var()
+
+        ax.axvline(x=historical_var / 100, color="red", linestyle="--", label=f"Historical VaR ({self.confidence_level*100:.0f}%)")
+        ax.axvline(x=parametric_var_normal / 100, color="green", linestyle="--", label="Parametric VaR (Normal)")
+        ax.axvline(x=parametric_var_t / 100, color="purple", linestyle="--", label="Parametric VaR (t-distribution)")
+        ax.axvline(x=monte_carlo_var / 100, color="orange", linestyle="--", label="Monte Carlo VaR")
+
+        ax.set_title(f"Portfolio Returns Distribution with VaR Confidence Intervals over {self.n_days} days")
+        ax.set_xlabel("Daily Return")
+        ax.set_ylabel("Frequency")
+        ax.legend()
+
+        # Display the plot using Streamlit
+        st.pyplot(fig)
